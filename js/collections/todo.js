a5befd827987dddfaafe9372de6a9b2f0e5adf07
euranium <mountrainier13@gmail.com>
2014-12-14 20:12:10 -0800

    updating dependencies

--- js/collections/todo.js
+++ js/collections/todo.js
@@ -1,33 +1,41 @@
 var app = app || {};
 
+// js/collections/todos.js
+// The collection of todos is backed by *localStorage* instead of a remote
+// server.
 var TodoList = Backbone.Collection.extend({
-	//Reference this collection's model
-	
-	model: app.Toddo,
-	//save all of the todos to todos-backbone namespace
-	localStorage: new Backbone.LocalStorage('todos-backbone'),
-	//filtering completed todos
-	completed: function(){
-		return this.filter(function( todo ){
-			return todo.get('complete');
-		});
-	},
-	//filter down list to only not finished
-	remaining: function(){
-		return this.without.apply( this, this.completed() );
-	},
-	//keep todos in sequential order
-	nextOrder: function(){
-		if( !this.length ) {
-			return 1;
-		}
-		return this.last().get('order') + 1;
-	},
-	//todos are sorted by their original order
-	comparator: function( todo ){
-		return todo.get('order');
-	}
+	// Reference to this collection's model.
+	model: app.Todo,
+
+  	// Save all of the todo items under the `"todos-backbone"` namespace.
+  	localStorage: new Backbone.LocalStorage('todos-backbone'),
+
+  	// Filter down the list of all todo items that are finished.
+  	completed: function() {
+    	return this.filter(function( todo ) {
+      	return todo.get('completed');
+    	});
+  	},
+
+  	// Filter down the list to only todo items that are still not finished.
+  	remaining: function() {
+    	return this.without.apply( this, this.completed() );
+  	},
+
+  	// We keep the Todos in sequential order, despite being saved by unordered
+  	// GUID in the database. This generates the next order number for new items.
+  	nextOrder: function() {
+    	if ( !this.length ) {
+      	return 1;
+    	}
+    	return this.last().get('order') + 1;
+  	},
+
+  	// Todos are sorted by their original insertion order.
+  	comparator: function( todo ) {
+    	return todo.get('order');
+  	}
 });
 
-//create new global collection of todos
+// Create our global collection of **Todos**.
 app.Todos = new TodoList();
