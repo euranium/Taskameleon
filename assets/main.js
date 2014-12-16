8a2cb78b47e7903c1ce4d0084db7ec8204404ea1
euranium <mountrainier13@gmail.com>
2014-12-15 22:42:11 -0800

    looks done

---
+++ assets/main.js
@@ -0,0 +1,23 @@
+$('#login-pic').mouseover(function(){
+	$( '#todoapp' ).fadeOut( 500, function(){
+		$( '#forms' ).fadeIn( 500, function(){
+			$('#forms').mouseleave(function(){
+				$('#forms').fadeOut(500, function(){
+					$('#todoapp').fadeIn(500)
+				});
+			});
+		});
+	});
+});
+
+$('#signin-pic').mouseover(function(){
+	$( '#todoapp' ).fadeOut( 500, function(){
+		$( '#forms' ).fadeIn( 500, function(){
+			$('#forms').mouseleave(function(){
+				$('#forms').fadeOut(500, function(){
+					$('#todoapp').fadeIn(500)
+				});
+			});
+		});
+	});
+});
